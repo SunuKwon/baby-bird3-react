2aab4f79a078618475bbaeea6f17f9036509cc15
hhpark <hhpark@ulogitech.com>
2025-09-21 19:21:21 +0900

    Input 컴포넌트 생성

--- src/componets/input/index.js
+++ src/componets/input/index.js
@@ -1,11 +1,13 @@
+import './index.css'
 
-export default function Input ({ type, value, onChange } ) {
+export default function Input ({ type, value, onChange, placeholder } ) {
 
     return (
         <input
             type={type}
             value={value}
             onChange={(e) => onChange(e.target.value) }
+            placeholder={placeholder}
         ></input>
     )
 }
